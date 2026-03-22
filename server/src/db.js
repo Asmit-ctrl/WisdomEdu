4a1b337f6ba7c92fb0858b4126ebede2a8cb94ae
Asmit-ctrl <Asmit-ctrl>
2026-03-22 16:26:07 +0530

    Remove legacy username index on connect

--- server/src/db.js
+++ server/src/db.js
@@ -2,9 +2,32 @@ import mongoose from "mongoose";
 import { config } from "./config.js";
 
 let connectionPromise = null;
+let legacyIndexCleanupPromise = null;
+
+async function cleanupLegacyIndexes(connection) {
+  if (legacyIndexCleanupPromise) {
+    return legacyIndexCleanupPromise;
+  }
+
+  legacyIndexCleanupPromise = (async () => {
+    const usersCollection = connection.collection("users");
+    const indexes = await usersCollection.indexes();
+    const hasLegacyUsernameIndex = indexes.some((index) => index.name === "username_1");
+
+    if (hasLegacyUsernameIndex) {
+      await usersCollection.dropIndex("username_1");
+    }
+  })().catch((error) => {
+    legacyIndexCleanupPromise = null;
+    throw error;
+  });
+
+  return legacyIndexCleanupPromise;
+}
 
 export async function connectDatabase() {
   if (mongoose.connection.readyState === 1) {
+    await cleanupLegacyIndexes(mongoose.connection);
     return mongoose.connection;
   }
 
@@ -14,7 +37,10 @@ export async function connectDatabase() {
 
   connectionPromise = mongoose
     .connect(config.mongodbUri)
-    .then(() => mongoose.connection)
+    .then(async () => {
+      await cleanupLegacyIndexes(mongoose.connection);
+      return mongoose.connection;
+    })
     .catch((error) => {
       connectionPromise = null;
       throw error;
